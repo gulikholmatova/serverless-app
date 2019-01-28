2b24d1db5ac1be0bf21ed55b6ebf51672bf5684f
Guli <gulikholmatova@gmail.com>
2019-01-27 21:13:04 -0500

    Updates

---
+++ precache-manifest.1409e1a7397db88acd328a6c7e94119b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1a5dd499d24dee59614",
+    "url": "/serverless-app/static/css/main.4fdca70d.chunk.css"
+  },
+  {
+    "revision": "e1a5dd499d24dee59614",
+    "url": "/serverless-app/static/js/main.e1a5dd49.chunk.js"
+  },
+  {
+    "revision": "bc050cb1fdfaddf34e40",
+    "url": "/serverless-app/static/js/1.bc050cb1.chunk.js"
+  },
+  {
+    "revision": "01b8580073d06bd6a560",
+    "url": "/serverless-app/static/js/runtime~main.01b85800.js"
+  },
+  {
+    "revision": "6eccb416096dca8c75d879bf1ac67f46",
+    "url": "/serverless-app/index.html"
+  }
+];
